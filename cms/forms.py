7f99e60b776b3c22228680a9de15cbd5bdb948ac
h_matsuda <h_matsuda@lepra.jp>
2020-02-18 11:40:04 +0900

    abort

--- cms/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from cms.models import Channel
-
-class ChannelForm(ModelForm):
-# Add Channel
-
-    class Meta:
-        model = Channel
-        fields = ('channelid', 'channeltitle',)
